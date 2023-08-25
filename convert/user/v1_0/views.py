146e9e6c7f79654d067803b0de517ca60a57b9bd
Lucas Ng <Lucas Ng>
2023-08-25 11:01:00 +0700

    fix response

--- convert/user/v1_0/views.py
+++ convert/user/v1_0/views.py
@@ -19,9 +19,13 @@ class ConvertRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ConvertSerializer
     permission_classes = []
     queryset = Converter.objects.all().order_by("-created_at")
-    
+
     def get(self, request, *args, **kwargs):
         response = super().get(request, *args, **kwargs)
-        response.data["converted_file"] = response.data["converted_file"].replace("http", "https")
+        try:
+            response.data["converted_file"] = response.data["converted_file"].replace(
+                "http", "https"
+            )
+        except:
+            pass
         return response
-
